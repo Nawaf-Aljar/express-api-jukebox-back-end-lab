6049720caa36069890cc18803b7e879fe44a10f0
Nawaf-Aljar <nawaf.aljar@outlook.com>
2025-12-29 11:40:13 +0300

    Added Delete Update and get one

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -13,5 +13,62 @@ router.post("/", async (req, res) => {
     }
 
 })
-
+router.get("/", async (req, res) => {
+    try {
+        const tracks = await Track.find({})
+        res.status(200).json({ tracks })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err: "Failed to get tracks" })
+    }
+})
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const track = await Track.findById(id)
+        if (!track) {
+            res.status(404).json({ err: "track not found" })
+        }
+        else {
+            res.status(200).json({ track })
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err: "Failed to get track" })
+    }
+})
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const track = await Track.findByIdAndDelete(id)
+        if (!track) {
+            res.status(404).json({ err: "track not found" })
+        }
+        else {
+            res.status(200).json({ track })
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err: "Failed to delete" })
+    }
+})
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const track = await Track.findByIdAndUpdate(id, req.body,{new: true})
+        if (!track) {
+            res.status(404).json({ error: "Track not found" })
+        }
+        else {
+            res.status(200).json({ track })
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Failed to update" })
+    }
+})
 module.exports = router
